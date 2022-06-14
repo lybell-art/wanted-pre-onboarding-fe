ed8f9c8555a24b74a004fa8065443d75fdc158d8
lybell <futireflugel@gmail.com>
2022-06-14 21:39:39 +0900

    add feeds style, infinity scrolls

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,27 +1,93 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import styled from 'styled-components';
 import NavBar from './components/NavBar.jsx';
 import Feed from './components/Feed.jsx';
 
-const LongDiv = styled.div`
-	width: 100%;
-	height: 9999px;
-	background-image: linear-gradient(180deg, #ffdfc5, #fe8093, #bf4243);
-	background-color: #fe8093;
+const MainDiv = styled.main`
+	display: flex;
+	flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 80px;
+    padding-bottom: 20px;
+    gap: 20px;
 `;
+
+const Intersector = styled.div`
+	display:absolute;
+	width:100%;
+	height:20px;
+	bottom:0px;
+`;
+
+
+function useIntersectionObserver(fetcher)
+{
+	const [itemLists, setItemLists] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const intersector = useRef();
+	const count = useRef(1);
+
+	useEffect(()=>console.log(itemLists), [itemLists]);
+
+	const getMoreItem = async ()=>{
+		setIsLoaded(true);
+		const newItems = await fetcher(count.current);
+		count.current++;
+		setItemLists(itemLists=>itemLists.concat(newItems));
+		setIsLoaded(false);
+	};
+
+	const onIntersect = async([entry], observer)=> {
+		if (entry.isIntersecting && !isLoaded) {
+			observer.unobserve(entry.target);
+			try{
+				await getMoreItem();
+				observer.observe(entry.target);
+			}
+			catch{
+				console.log('end of files!');
+			}
+		}
+	};
+
+	useEffect(()=>{
+		let observer;
+		if (intersector.current) {
+			observer = new IntersectionObserver(onIntersect, {
+				rootMargin: '0px 0px 50px 0px',
+				threshold: 0.5
+			});
+			observer.observe(intersector.current);
+		}
+		return ()=>observer && observer.disconnect();
+	}, [intersector]);
+
+	return [itemLists, intersector];
+}
 
 function Main()
 {
+	const loadMore = (i)=>{
+		return fetch(`/data/feeds${i}.json`)
+			.then(response=>response.json());
+	}
+
+	let [feeds, intersector] = useIntersectionObserver(loadMore);
+
 	return <>
-		<NavBar></NavBar>
-		<main>
-			<Feed 
-				name="yes" 
-				date={1650000000000} 
-				image="https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg" 
-				content="test" 
-			/>
-		</main>
+		<NavBar />
+		<MainDiv>
+			{feeds.map( ({who, upload_date, img, content}, i)=>{
+				return <Feed key={`${who}_${upload_date}_${i}`} 
+					name={who}
+					date={upload_date}
+					image={img}
+					content={content}
+				/>
+			} )}
+			<Intersector ref={intersector} />
+		</MainDiv>
 	</>;
 }
 
--- src/pages/components/Feed.jsx
+++ src/pages/components/Feed.jsx
@@ -1,4 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	width: 60%;
+    padding: 20px;
+    border: 1px solid #eeee;
+    box-shadow: 2px 2px 6px #0002;
+`;
+
+const TitleArea = styled.div`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	font-weight: bold;
+	gap: 10px;
+	margin-bottom: 10px;
+`;
+
+const Profile = styled.div`
+	width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-image: linear-gradient(360deg, #ff8080, #ffd23e);
+`;
+
+const NameSpace = styled.p`
+	transform: translateY(-0.125em);
+`;
+
+
+const ImagePlaceholder = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+    align-items: center;
+`;
+
+const Img = styled.img`
+	max-width: 100%;
+`;
+
+const DateText = styled.p`
+    text-align: end;
+    font-size: 0.75em;
+    color: #aaa;
+`;
+
+const Description = styled.div`
+    width: 100%;
+    min-height: 100px;
+`;
+
 
 function Feed({ name, date, image, content })
 {
@@ -12,13 +66,13 @@ function Feed({ name, date, image, content })
 		}
 	}, []);
 
-	if (imgSrc === null) return <div>Loading...</div>;
-	return <div>
-		<div>{name}</div>
-		<img src={image} alt="image"/>
-		<div>{new Date(date).toLocaleString()}</div>
-		<div>{content}</div>
-	</div>
+	if (imgSrc === null) return <Wrapper>Loading...</Wrapper>;
+	return <Wrapper>
+		<TitleArea><Profile /><NameSpace>{name}</NameSpace></TitleArea>
+		<ImagePlaceholder><Img src={image} alt="image"/></ImagePlaceholder>
+		<DateText>{new Date(date).toLocaleString()}</DateText>
+		<Description>{content}</Description>
+	</Wrapper>
 }
 
 export default Feed;
